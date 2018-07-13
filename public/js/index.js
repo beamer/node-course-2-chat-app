c586a837bb12efc9e2ee11f5c12fffea36a53e45
Suthep <suthepsvp@hotmail.com>
2018-07-13 09:54:12 +0700

    Add new Message and create Message event

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+var socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server');
+  // socket.emit('createEmail', {
+  //   to: 'jen@example.com',
+  //   text: 'Hey. This is Andrew'
+  // });
+
+  socket.emit('createMessage', {
+    from: 'Andrew',
+    text: 'Yup, that works for me'
+  });
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnect from server');
+});
+
+// socket.on('newEmail', function(email) {
+//   console.log('New email ', email);
+// });
+
+socket.on('newMessage', function(message) {
+  console.log('Messagex: ', message)
+})
--- server/server.js
+++ server/server.js
@@ -13,8 +13,28 @@ try {
   io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit('newEmail', {
+  //   from: 'mike@example.com',
+  //   text: 'Hey. what is going on.',
+  //   createdAt: 123
+  // });
+
+  socket.emit('newMessage', {
+    from: 'John',
+    text: 'See you then',
+    createdAt: 123123
+  });
+
+  socket.on('createEmail', (newEmail) => {
+    console.log('create email ', newEmail);
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('Message: ', message)
+  });
+
   socket.on('disconnect', (socket) => {
-    console.log('User was disconnected');
+    console.log('User was disconnect');
   });
 })} catch(error) {
   console.log('unable to connect');
