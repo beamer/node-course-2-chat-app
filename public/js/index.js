0eae7701a0bba0aa9a7c8f6096405de2bc24044f
Suthep <suthepsvp@hotmail.com>
2018-07-13 16:03:17 +0700

    Emit new Measage

--- public/js/index.js
+++ public/js/index.js
@@ -2,25 +2,47 @@ var socket = io();
 
 socket.on('connect', function() {
   console.log('Connected to server');
-  // socket.emit('createEmail', {
-  //   to: 'jen@example.com',
-  //   text: 'Hey. This is Andrew'
+});
+
+  socket.on('disconnect', function() {
+    console.log('Disconnect from server');
+  });
+
+  socket.on('newMessage', function (message) {
+    console.log('Messagex: ', message);
+    var li = jQuery('<li></li>');
+    try {
+      var xx = `${message.from}:  ${message.text}`;
+    li.text(`${message.from}: ${message.text}`);
+
+    console.log('inside index.js new message ', li, ' : ', xx);
+    jQuery('#messages').append(li);
+  } catch (e) {
+    console.log('error');
+  }
+  });
+
+
+  // socket.emit('createMessage', {
+  //   from: 'Frank',
+  //   text: 'Hi'
+  // }, function(data) {
+  //   console.log('Got it', data);
   // });
 
-  socket.emit('createMessage', {
-    from: 'Andrew',
-    text: 'Yup, that works for me'
-  });
-});
-
-socket.on('disconnect', function() {
-  console.log('Disconnect from server');
-});
 
 // socket.on('newEmail', function(email) {
 //   console.log('New email ', email);
 // });
 
-socket.on('newMessage', function(message) {
-  console.log('Messagex: ', message)
-})
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  console.log('just click ' , jQuery('[name=message]').val());
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
+});
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const socketIO = require("socket.io");
+//const {generateMessage} = require('../messages/message');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 2500;
@@ -19,18 +20,27 @@ try {
   //   createdAt: 123
   // });
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
-  });
+  //socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
-  socket.on('createEmail', (newEmail) => {
-    console.log('create email ', newEmail);
-  });
+  //socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
+  // socket.emit('newMessage', {
+  //   from: 'John',
+  //   text: 'See you then',
+  //   createdAt: 123123
+  // });
+
+  // socket.on('createEmail', (newEmail) => {
+  //   console.log('create email ', newEmail);
+  // });
 
   socket.on('createMessage', (message) => {
-    console.log('Message: ', message)
+    console.log('Message: ', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+    //io.emit('newMessage', generateMessage(message.from, message.text));
   });
 
   socket.on('disconnect', (socket) => {
@@ -41,8 +51,6 @@ try {
 };
 
 app.use(express.static(publicPath));
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
